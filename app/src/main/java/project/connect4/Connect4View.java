04f58a7ae54d5214dac50cfadbadec739737021b
Pilihp64 <prbriley@fullsail.edu>
2017-11-02 16:28:28 -0400

    Chips fall down, fade out win screens, pause/resume works better, added comments

--- app/src/main/java/project/connect4/Connect4View.java
+++ app/src/main/java/project/connect4/Connect4View.java
@@ -40,18 +40,22 @@ public class Connect4View extends SurfaceView implements Runnable {
     //boolean variable to track if the game is playing or not
     volatile boolean playing;
 
+    //Have we setup the static vars?
+    private static boolean setup = false;
+
     //the game thread
     private Thread gameThread = null;
 
+    //A list of all events this game uses
     private static String[] events = {"Chip_Placed", "Blue_Chip_Placed", "Red_Chip_Placed", "Blue_Wins", "Red_Wins", "Tie_Game"};
-    private static boolean setup = false;
 
-    private Paint paint;
     private Paint fontPaint;
-    private Paint gameOverPaint;
+    private static Paint gameOverPaint;
     private static Canvas canvas;
     private SurfaceHolder surfaceHolder;
 
+    //Images
+    private static Bitmap background;
     private static Bitmap chipRed;
     private static Bitmap chipBlue;
     private static Bitmap chipYellow;
@@ -59,21 +63,27 @@ public class Connect4View extends SurfaceView implements Runnable {
     private static Bitmap blueWins_image;
     private static Bitmap redWins_image;
 
-
+    //Temporary yellow chip location
     private float tmpX = -100;
     private float tmpY = -100;
 
-    private static boolean redsTurn = true;
-    private static boolean gameOver = false;
-    private static boolean redWins = false;
-    private static boolean blueWins = false;
+    //Booleans for game states
+    private static boolean redsTurn;
+    private static boolean gameOver;
+    private static boolean redWins;
+    private static boolean blueWins;
+
+    //The sound for chip placement
     private static MediaPlayer mp;
-    MapGrid<Chip> mapGrid;
+
+    //The main grid which holds the chips
+    private static MapGrid<Chip> mapGrid;
 
     private static void onChip_Placed()
     {
         //Play a sound effect
         mp.start();
+
         //Change turns after a chip was played
         redsTurn = !redsTurn;
     }
@@ -104,41 +114,53 @@ public class Connect4View extends SurfaceView implements Runnable {
         gameOver = true;
         //Add some stats
     }
+
     //Class constructor
     public Connect4View(Context context) {
         super(context);
         surfaceHolder = getHolder();
-        paint = new Paint();
         fontPaint = new Paint();
         fontPaint.setTextSize(45f);
-        gameOverPaint = new Paint();
-        gameOverPaint.setTextSize(360f);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inScaled = false;
 
-        Bitmap background = BitmapFactory.decodeResource(context.getResources(),R.drawable.connect4board, options);
 
-        mapGrid = new MapGrid<Chip>(7,6, background, new Rect(14,14,12,12));
-
+        //The first time this is made, setup statics
         if (!setup)
         {
-            mp = MediaPlayer.create(context,R.raw.chip_click);
-
-            Bitmap sprite = BitmapFactory.decodeResource(context.getResources(),R.drawable.connect4chips, options);
-            chipRed = Bitmap.createBitmap(sprite,0,0,150,150);
-            chipBlue = Bitmap.createBitmap(sprite,150,0,150,150);
-            chipYellow = Bitmap.createBitmap(sprite,0,150,150,150);
-
-            blueWins_image = BitmapFactory.decodeResource(context.getResources(),R.drawable.winnerblue, options);
-            redWins_image = BitmapFactory.decodeResource(context.getResources(),R.drawable.winnerred, options);
-
-            setupOnce();
+            setupOnce(context);
+            newGame();
         }
 
     }
-    private static void setupOnce()
+    private static void setupOnce(Context context)
     {
         setup = true;
+
+        //Image loading options
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        //Don't scale up images
+        options.inScaled = false;
+
+        //The main game background
+        background = BitmapFactory.decodeResource(context.getResources(),R.drawable.connect4board, options);
+
+        //The click sound
+        mp = MediaPlayer.create(context,R.raw.chip_click);
+
+        //Load piece image, and split it into colors
+        Bitmap sprite = BitmapFactory.decodeResource(context.getResources(),R.drawable.connect4chips, options);
+        chipRed = Bitmap.createBitmap(sprite,0,0,150,150);
+        chipBlue = Bitmap.createBitmap(sprite,150,0,150,150);
+        chipYellow = Bitmap.createBitmap(sprite,0,150,150,150);
+
+        //End game images
+        blueWins_image = BitmapFactory.decodeResource(context.getResources(),R.drawable.winnerblue, options);
+        redWins_image = BitmapFactory.decodeResource(context.getResources(),R.drawable.winnerred, options);
+
+        //Alpha paint for gameover
+        gameOverPaint = new Paint();
+        gameOverPaint.setTextSize(360f);
+        gameOverPaint.setColor(Color.argb(127,0,0,0));
+
         //Clear any events from before
         EventSystem.clearEvents();
         //Add our event names
@@ -154,12 +176,14 @@ public class Connect4View extends SurfaceView implements Runnable {
         EventSystem.addHook("Blue_Wins",Connect4View::onBlue_Wins);
         EventSystem.addHook("Tie_Game",Connect4View::onTie_Game);
     }
+    //Setup a new round, generates an empty grid
     private void newGame()
     {
         gameOver = false;
         redsTurn = true;
         redWins = false;
         blueWins = false;
+        mapGrid = new MapGrid<Chip>(7,6, background, new Rect(14,14,12,12));
     }
 
     @Override
@@ -168,14 +192,17 @@ public class Connect4View extends SurfaceView implements Runnable {
         float x = e.getX();
         float y = e.getY();
 
-        switch (e.getAction()) {
-
+        switch (e.getAction())
+        {
+            //Our main chip placement
             case MotionEvent.ACTION_DOWN:
                 MapGrid<Chip>.Coord tmp = mapGrid.getCoordOfTouch((int)x,(int)y);
                 if (tmp.x >= 0 && tmp.x < 7)
                 {
                     addChip(redsTurn,tmp.x);
                 }
+                //No break to update the yellow chip location
+                //We can get continued movement here
             case MotionEvent.ACTION_MOVE:
                 tmpX = x;
                 tmpY = y;
@@ -186,46 +213,34 @@ public class Connect4View extends SurfaceView implements Runnable {
     }
     public boolean addChip(boolean red, int column)
     {
-        if (gameOver)
+        if (gameOver || isFalling)
             return false;
-        MapGrid<Chip>.Node target = mapGrid.getNodeCoord(0,0);
-
-        for (int i = 0; i < column; ++i)
-        {
-            target = target.right;
-        }
+        MapGrid<Chip>.Node target = mapGrid.getNodeCoord(column,0);
 
         if (target.data != null)
             return false;
 
-        while (true)
-        {
-            if (target.down == null || target.down.data != null)
-                break;
-            target = target.down;
-        }
-
         target.data = new Chip(red?chipRed:chipBlue, red);
 
-
-        if (winCheck(target))
+        if (target.down.data != null)
         {
-            if (redsTurn)
-                EventSystem.triggerEvent("Red_Wins");
+            EventSystem.triggerEvent("Chip_Placed");
+            if (target.data.Red())
+                EventSystem.triggerEvent("Red_Chip_Placed");
             else
-                EventSystem.triggerEvent("Blue_Wins");
-        }
-        else if (tieCheck())
-        {
-            EventSystem.triggerEvent("Tie_Game");
-        }
+                EventSystem.triggerEvent("Blue_Chip_Placed");
 
-        EventSystem.triggerEvent("Chip_Placed");
-        if (red)
-            EventSystem.triggerEvent("Red_Chip_Placed");
+            //If we can't fall, it could be a tie
+            if (tieCheck())
+            {
+                EventSystem.triggerEvent("Tie_Game");
+            }
+        }
         else
-            EventSystem.triggerEvent("Blue_Chip_Placed");
-
+        {
+            startFalling = true;
+            isFalling = true;
+        }
 
         return true;
     }
@@ -240,9 +255,78 @@ public class Connect4View extends SurfaceView implements Runnable {
             sleep();
         }
     }
+    private static void fallDownTiles()
+    {
+        isFalling = false;
+        //Go through every width
+        MapGrid<Chip>.Node width = mapGrid.getNodeCoord(0,4);
+        while (width != null)
+        {
+            //Go through every height
+            MapGrid<Chip>.Node check = width;
+            while (check != null)
+            {
+                //Can we move down?
+                if (check.data != null && check.down.data == null)
+                {
+                    //Move it down here
+                    check.down.data = check.data;
+                    check.data = null;
+                    isFalling = true;
 
+                    if (check.down.down == null || check.down.down.data != null) {
+                        EventSystem.triggerEvent("Chip_Placed");
+                        if (check.down.data.Red())
+                            EventSystem.triggerEvent("Red_Chip_Placed");
+                        else
+                            EventSystem.triggerEvent("Blue_Chip_Placed");
+                    }
+                }
+                check = check.up;
+            }
+            width = width.right;
+        }
+
+    }
+    //If something fell last frame
+    private static boolean isFalling = false;
+    //If we should start falling next frame
+    private static boolean startFalling = false;
+    //A previous falling state to see when we are done
+    private static boolean wasFalling = false;
     private void update() {
+        wasFalling = isFalling;
 
+        if (isFalling && !startFalling)
+            fallDownTiles();
+
+        //Everything is done moving, check wins
+        if (wasFalling && !isFalling)
+        {
+            //Check wins
+            MapGrid<Chip>.Node tmp = mapGrid.getNodeCoord(0,0);
+            while (tmp != null)
+            {
+                MapGrid<Chip>.Node tmp2 = tmp;
+                while (tmp2 != null) {
+                    if (winCheck(tmp2)) {
+                        if (tmp2.data.Red())
+                            EventSystem.triggerEvent("Red_Wins");
+                        else
+                            EventSystem.triggerEvent("Blue_Wins");
+                        return;
+                    }
+                    tmp2 = tmp2.down;
+                }
+
+                tmp = tmp.right;
+            }
+        }
+        //Delay the fall for one update
+        if (startFalling)
+        {
+            startFalling = false;
+        }
     }
 
     public static void DrawChip(Chip c, MapGrid<Chip>.Coord loc)
@@ -262,34 +346,37 @@ public class Connect4View extends SurfaceView implements Runnable {
 
             mapGrid.Draw(canvas, this, chipDraw);
 
-            canvas.drawBitmap(chipYellow,tmpX-75,tmpY-75,paint);
+            canvas.drawBitmap(chipYellow,tmpX-75,tmpY-75,null);
 
             if (redsTurn) {
-                canvas.drawBitmap(chipRed, 25, 25, paint);
+                canvas.drawBitmap(chipRed, 25, 25, null);
                 canvas.drawText("RED Turn",5,215,fontPaint);
 
             }
             else {
-                canvas.drawBitmap(chipBlue, getWidth() - 175, 25, paint);
+                canvas.drawBitmap(chipBlue, getWidth() - 175, 25, null);
                 canvas.drawText("BLUE Turn",getWidth() - 225,215,fontPaint);
             }
 
             if (gameOver)
             {
                 if (redWins) {
-                    //gameOverPaint.setColor(Color.RED);
-                    //canvas.drawText("Red Wins!", 50, 615, gameOverPaint);
-                    canvas.drawBitmap(redWins_image,null,new Rect(50,50,getWidth()-50,getHeight()-50),paint);
+                    canvas.drawRect(0,0,getWidth(),getHeight(),gameOverPaint);
+                    canvas.drawBitmap(redWins_image,null,new Rect(50,50,getWidth()-50,getHeight()-50),null);
+                    //gameOverPaint.setColor(Color.argb(127,0,0,0));
+
                 }
                 else if (blueWins) {
-                    //gameOverPaint.setColor(Color.BLUE);
-                    //canvas.drawText("Blue Wins!", 50, 615, gameOverPaint);
-                    canvas.drawBitmap(blueWins_image,null,new Rect(50,50,getWidth()-50,getHeight()-50),paint);
+                    canvas.drawRect(0,0,getWidth(),getHeight(),gameOverPaint);
+                    canvas.drawBitmap(blueWins_image,null,new Rect(50,50,getWidth()-50,getHeight()-50),null);
+                    //gameOverPaint.setColor(Color.argb(127,0,0,0));
 
                 }
                 else {
+                    canvas.drawRect(0,0,getWidth(),getHeight(),gameOverPaint);
                     gameOverPaint.setColor(Color.YELLOW);
                     canvas.drawText("Tie Game!", 50, 615, gameOverPaint);
+                    gameOverPaint.setColor(Color.argb(127,0,0,0));
                 }
             }
 
@@ -327,7 +414,7 @@ public class Connect4View extends SurfaceView implements Runnable {
         gameThread = new Thread(this);
         gameThread.start();
     }
-    boolean tieCheck()
+    static boolean tieCheck()
     {
         MapGrid<Chip>.Node tmp = mapGrid.getNodeCoord(0,0);
         while (tmp != null) {
@@ -339,7 +426,7 @@ public class Connect4View extends SurfaceView implements Runnable {
         }
         return false;
     }
-    boolean winCheck(MapGrid<Chip>.Node check)
+    static boolean winCheck(MapGrid<Chip>.Node check)
     {
         MapGrid<Chip>.Node tmp = check;
         int count = 0;
--- app/src/main/java/project/connect4/EventSystem.java
+++ app/src/main/java/project/connect4/EventSystem.java
@@ -8,9 +8,12 @@ import java.util.Map;
  */
 
 public class EventSystem {
+    //The interface that hooks will be calling (A void/void function)
     public interface HookInterface{
         void poke();
     }
+
+    //The main list of events
     private static Map<String,List<HookInterface>> events = new HashMap<>();
 
     EventSystem()
@@ -18,22 +21,28 @@ public class EventSystem {
         clearEvents();
     }
 
+    //Clear events
     public static void clearEvents()
     {
         events = new HashMap<>();
     }
 
+    //Add a new event name
     public static void initEvent(String name)
     {
         //events.putIfAbsent(name,new ArrayList<>());
         if (!events.containsKey(name))
             events.put(name,new ArrayList<>());
     }
+
+    //Add a new hook to existing event name
     public static void addHook(String name, HookInterface hook)
     {
         if (events.containsKey(name))
             events.get(name).add(hook);
     }
+
+    //Call all hooks attached to an event name
     public static void triggerEvent(String name)
     {
         //events.putIfAbsent(name,new ArrayList<>());
